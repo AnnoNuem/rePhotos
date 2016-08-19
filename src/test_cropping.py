c7181da77beeafaee07134a19b04123caff6dd5d
aschaffland <aschaffland@uos.de>
2016-08-19 10:20:27 +0200

    bugfix in new cropping

--- src/test_cropping.py
+++ src/test_cropping.py
@@ -3,6 +3,7 @@ import numpy as np
 from image_helpers import get_crop_indices
 img = cv2.imread("test_crop.jpg")
 x_min, x_max, y_min, y_max = get_crop_indices(img)
+print x_min, x_max, y_min, y_max
 cv2.namedWindow("img1", cv2.WINDOW_KEEPRATIO)
 cv2.namedWindow("img2", cv2.WINDOW_KEEPRATIO)
 cv2.imshow("img1", img[y_min: y_max, x_min: x_max,:])
