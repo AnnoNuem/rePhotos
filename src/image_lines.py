978e52d57a8c93fe86378fc65b64c094e085490c
aschaffland <aschaffland@uos.de>
2016-11-07 20:20:15 +0100

    cleaned image_lines.py

--- src/image_lines.py
+++ src/image_lines.py
@@ -2,18 +2,20 @@ import numpy as np
 import cv2
 from image_helpers import statistic_canny
 from image_helpers import adaptive_thresh
-#from image_helpers import pst_wrapper
 from image_helpers import line_intersect 
 
 # Patch Size Divisor: Area arround line in which similar line is searched
-psd = 60
+psd = 70
 scale_factor = 2
 
 ADAPT_THRESH = 0
 STAT_CANNY = 1
 PST = 2
 
+#TODO gaus on line image
 #TODO lce
+#TODO check if comparison between user drawn and hough line is required
+
 
 def lim_line_length(p1_h, p2_h, p1_o, p2_o):
     """
@@ -65,6 +67,58 @@ def get_theta(p1, p2):
         return np.arctan(m) + np.pi/2
 
 
+def weight_lines(patch_p, lines, p1_o, p2_o):
+    """
+    Weights hough lines by similarity to edges.
+    Generates line segments with length according to user drawn lines and 
+    compares segments with edges in edge image. Select best matching line.
+    Only lines with similar orientation to user drawn line are regarded.
+    Only best ten lines fullfilling above criterium are considered.
+
+    Args:
+        patch_p: Edgeimage.
+        lines: List of lines.
+        p1_o: Startpoint of user drawn line.
+        p2_o: Endpoint of user drawn line.
+
+    Returns:
+        p1: Startpoint of best matching line.
+        p2: Endpoint of best matching line.
+    """
+    #iterate over max ten best lines
+    nl = min(10, lines.shape[0])
+    line_img = np.empty(patch_p.shape, dtype=np.uint8)
+    weights = np.empty((nl), dtype=np.float32)
+    line_segs = np.empty((nl), dtype=object)
+    i, j = 0, 0
+    theta_o = get_theta(p1_o, p2_o)    
+
+    while  j < lines.shape[0] and i < 10 :
+        rho = lines[j][0][0]
+        theta = lines[j][0][1]
+        j += 1
+        # allow only lines with roughly the same slope as user drawn lines
+        if np.abs(theta_o - theta) < .05 or np.pi - np.abs(theta_o - theta) < 0.05:
+            line_img[:] = 0
+            a = np.cos(theta)
+            b = np.sin(theta)
+            x0, y0 = a * rho, b * rho
+            pt1 = (int(x0 + 1000 * (-b)), int(y0 + 1000 * a))
+            pt2 = (int(x0 - 1000 * (-b)), int(y0 - 1000 * a))
+            pt1, pt2 = lim_line_length(pt1, pt2, p1_o, p2_o)
+            cv2.line(line_img, (pt1[0], pt1[1]), (pt2[0], pt2[1]), 255, 1, 4)
+            weights[i] = np.einsum('ij,ij->', line_img, patch_p)
+            line_segs[i] = (pt1, pt2)
+            i += 1
+
+    if i > 0:
+        k = weights[:i].argmax()
+        p1 = line_segs[k][0] 
+        p2 = line_segs[k][1] 
+
+    return p1, p2
+
+
 def get_line(p1, p2, img, method):
     """
     Search in image for most similar line of a given line.
@@ -82,13 +136,14 @@ def get_line(p1, p2, img, method):
     length = np.sqrt((p1[1] - p2[1]) * (p1[1] - p2[1]) + (p1[0] - p2[0]) * (p1[0] - p2[0])) 
     v = np.array([(p2[1] - p1[1])/length, (p2[0] - p1[0])/length])
 
-    # Compute horizontal rectangle containing the patch arround the given line
+    # Compute rectangle with line in the middle and same rotation as line
+    # and bounding box arround it
     s = (img.shape[0] + img.shape[1])/ (2 * psd)
     m1 = p1 + s * (v * [ 1, -1] - v[::-1]) 
     m2 = p1 + s * (v * [ -1, 1] - v[::-1]) 
     m3 = p2 + s * (v * [ -1, 1] + v[::-1])
     m4 = p2 + s * (v * [ 1, -1] + v[::-1])
-    p = np.array([m1, m2, m3, m4])
+    p = np.array([m1, m2, m3, m4], dtype=np.uint32)
     x_min = int(max(0, p.T[1,:].min()))
     x_max = int(min(img.shape[0], p.T[1,:].max()))
     y_min = int(max(0, p.T[0,:].min()))
@@ -117,124 +172,12 @@ def get_line(p1, p2, img, method):
     p2_o = p2 - offset
 
     # Line detection
-    p_x_min = min(p1_o[1],p2_o[1])
-    p_x_max = max(p1_o[1],p2_o[1])
-    p_y_min = min(p1_o[0],p2_o[0])
-    p_y_max = max(p1_o[0],p2_o[0])
-    min_line_length = np.sqrt((p_x_max - p_x_min) * (p_x_max - p_x_min) + (p_y_max - p_y_min) * (p_y_max - p_y_min)) * 0.5 
-    max_gap = min_line_length/2
-    cv2.imshow('linet', patch_p)
-    #lines = cv2.HoughLinesP(patch_p, 1, np.pi/180.0, 1, np.array([]), min_line_length, max_gap)
-    lines = cv2.HoughLines(patch_p, 1, np.pi/180.0, 10, np.array([]), 0, 0)
+    lines = cv2.HoughLines(patch_p, 1, np.pi/180.0, 1, np.array([]), 0, 0)
 
-
-    # return given points if no lines found, else:
+    # return given points if no lines found, else compute best line
     if lines is not None:
-        ### hough standard
-        if True:
-            # Generate weight image with values decreasing with distance from given line
-            weight_img = np.ones(patch_p.shape, dtype=np.uint8)
-            cv2.line(weight_img, (p1_o[0], p1_o[1]), (p2_o[0], p2_o[1]), 0, 1, 4)
-            # Missing in python
-            CV_DIST_MASK_PRECISE = 0 
-            CV_DIST_L2 = 2
-            weight_img = cv2.distanceTransform(weight_img, CV_DIST_L2, CV_DIST_MASK_PRECISE)
-            w_min = weight_img.min()
-            w_max = weight_img.max()
-            weight_img = (1 - (weight_img - w_min)/(w_min - w_max)) * mask
-
-            #iterate over max ten best lines
-            nl = lines.shape[0]
-            nl = min(nl, 10)
-            tmp_img = np.zeros(patch_p.shape, dtype=np.uint8)
-            line_img = np.empty(patch_p.shape, dtype=np.uint8)
-            weights_patch = np.empty((nl), dtype=np.float32)
-            weights_user = np.empty((nl), dtype=np.float32)
-            line_segs = np.empty((nl), dtype=object)
-            i, j = 0, 0
-            theta_o = get_theta_o(p1_o, p2_o)    
-            while  j < lines.shape[0] and i < 10 :
-                rho = lines[j][0][0]
-                theta = lines[j][0][1]
-                j += 1
-
-                # allow only lines with roughly the same slope as user drawn lines
-                if np.abs(theta_o - theta) < .05 or np.pi - np.abs(theta_o - theta) < 0.05:
-                    line_img[:] = 0
-                    a = np.cos(theta)
-                    b = np.sin(theta)
-                    x0, y0 = a * rho, b * rho
-                    # TODO change 1000 to pshape
-                    pt1 = (int(x0 + 1000 * (-b)), int(y0 + 1000 * a))
-                    pt2 = (int(x0 - 1000 * (-b)), int(y0 - 1000 * a))
-                    pt1, pt2 = lim_line_length(pt1, pt2, p1_o, p2_o)
-                    cv2.line(tmp_img, (pt1[0], pt1[1]), (pt2[0], pt2[1]), 255, 1, 4)
-                    cv2.line(line_img, (pt1[0], pt1[1]), (pt2[0], pt2[1]), 255, 1, 4)
-                    weights_patch[i] = np.einsum('ij,ij->', line_img, patch_p)
-                    weights_user[i] = np.einsum('ij,ij->', line_img, weight_img)
-                    line_segs[i] = (pt1, pt2)
-                    i += 1
-            weights_user = (weights_user - weights_user.min()) / (weights_user.max() - weights_user.min())
-            weights_patch= (weights_patch - weights_patch.min()) / (weights_patch.max() - weights_patch.min())
-            #print weights_user
-            #print weights_patch
-            weights = weights_patch #+ 0.5 * weights_user
-            #print weights[:i]
-            #print
-            if i > 0:
-                k = weights[:i].argmax()
-                p1 = line_segs[k][0] + offset
-                p2 = line_segs[k][1] + offset
-            
-        
-        ###  hough probabilistic
-        if False:
-            # Generate weight image with values decreasing with distance from given line
-            weight_img = np.ones(patch_p.shape, dtype=np.uint8)
-            cv2.line(weight_img, (p1_o[0], p1_o[1]), (p2_o[0], p2_o[1]), 0, 1, 4)
-            # Missing in python
-            CV_DIST_MASK_PRECISE = 0 
-            CV_DIST_L2 = 2
-            weight_img = cv2.distanceTransform(weight_img, CV_DIST_L2, CV_DIST_MASK_PRECISE)
-            w_min = weight_img.min()
-            w_max = weight_img.max()
-            weight_img = (1 - (weight_img - w_min)/(w_min - w_max)) * mask
-
-            # Iterate over lines and find most similar line
-            a,b,c = lines.shape
-            line_img = np.empty(weight_img.shape, dtype=np.uint8)
-            weights = np.empty((a), dtype=np.float32)
-            tmp_img = np.zeros(weight_img.shape, dtype=np.uint8)
-            for i in range(a):
-                line_img[:] = 0
-                cv2.line(line_img, (lines[i][0][0], lines[i][0][1]), (lines[i][0][2], lines[i][0][3]), 1, 1, 4)
-                cv2.line(tmp_img, (lines[i][0][0], lines[i][0][1]), (lines[i][0][2], lines[i][0][3]), 255, 1, 4)
-                # Multiply line_img with weight_img and sum all entries
-                weights[i] = np.einsum('ij,ij->', line_img, weight_img)
-        
-        
-        if False:
-            a,b,c = lines.shape
-            weights = np.empty((a), dtype=np.float32)
-            tmp_img = np.zeros(weight_img.shape, dtype=np.uint8)
-            for i in range(a):
-                line_img = np.zeros(patch_p.shape, dtype=np.uint8)
-                cv2.line(line_img, (lines[i][0][0], lines[i][0][1]), (lines[i][0][2], lines[i][0][3]), 255, 5, 4)
-                cv2.line(line_img2, (lines[i][0][0], lines[i][0][1]), (lines[i][0][3], -lines[i][0][2]), 255, 5, 4)
-                cv2.line(tmp_img, (lines[i][0][0], lines[i][0][1]), (lines[i][0][2], lines[i][0][3]), 255, 1, 4)
-                #line_img = cv2.distanceTransform(line_img, CV_DIST_L2, CV_DIST_MASK_PRECISE)
-                #line_img = line_img - line_img.min()
-                #line_img = (1 - line_img/line_img.max()) * mask
-                cv2.imshow('baf', line_img)
-                cv2.imshow('baf2', line_img2)
-                cv2.waitKey(0)
-                # Multiply line_img with weight_img and sum all entries
-                weights[i] = np.einsum('ij,ij->', line_img, patch_p)
-        
-
-
-        cv2.imshow('hough', tmp_img)
-
-
+        p1, p2 = weight_lines(patch_p, lines, p1_o, p2_o)
+        p1 += offset
+        p2 += offset
     
     return [p1[0], p1[1], p2[0], p2[1]]
