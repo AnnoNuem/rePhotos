498fe03ff7e82a89d16f1affc440fc8c8d42ab9d
aschaffland <aschaffland@uos.de>
2017-02-07 22:14:37 +0100

    corresponingpoint works better

--- src/image_sac.py
+++ src/image_sac.py
@@ -1,11 +1,12 @@
 import cv2
 import numpy as np
 from image_helpers import lce
+from image_helpers import draw_circle
 
 # scale factor for subimage size on which template matching is done
-SUBIMAGE_SIZE_SCALE = 0.159  
+SUBIMAGE_SIZE_SCALE = 0.5 #0.159  
 # scale factor for template size
-TEMPLATE_SIZE_SCALE = 0.07
+TEMPLATE_SIZE_SCALE = 0.1
 # minimum size of user drawn rectangle to start point search else return middle point
 MIN_RECT_SIZE = 5
 
@@ -61,7 +62,16 @@ def getPointFromRectangle(img1, point1, point2):
 
     return returnPoint
 
-def getCorespondingPoint(img1, img2, point):
+
+def get_and_pre_patch(img, point, size_half):
+    x1 = max(point[0] - size_half, 0)
+    x2 = min(point[0] + size_half, img.shape[1] - 1)
+    y1 = max(point[1] - size_half, 0)
+    y2 = min(point[1] + size_half, img.shape[0] - 1)
+    subimageF1 = cv2.cvtColor(np.float32(img[y1:y2, x1:x2]), cv2.COLOR_BGR2HSV)[...,2]
+    return cv2.GaussianBlur(subimageF1, (5,5), 0), np.array((x1, y1))   
+
+def getCorespondingPoint(img1, img2, point, template_size_s=101):
     """Search for coresponding point on second image given a point in first image using template matching."""
 
     if not (0 <= point[1] < img1.shape[0]) or\
@@ -71,91 +81,70 @@ def getCorespondingPoint(img1, img2, point):
        return None
 
     point = np.array(point, dtype=np.int_)
+    
     # diameter of the meaningfull keypoint neighborhood 
-    templateSize = int((img1.shape[0] + img1.shape[1]) * 0.5  * TEMPLATE_SIZE_SCALE)
+    templateSizeHalf = int(((img1.shape[0] + img1.shape[1]) * 0.5  * \
+                              TEMPLATE_SIZE_SCALE)/2)
 
     # size of local subimage in which dense sampling is done
-    subimageSize = int((img2.shape[0] + img2.shape[1]) * 0.5 * SUBIMAGE_SIZE_SCALE)
+    subimageSizeHalf = int(((img2.shape[0] + img2.shape[1]) * 0.5 * \
+                             SUBIMAGE_SIZE_SCALE)/2)
 
     # get template from img1 in which user draw
-    templateSizeHalf = int(templateSize/2)
-    x1 = max(point[0] - templateSizeHalf, 0)
-    x2 = min(point[0] + templateSizeHalf, img1.shape[1] - 1)
-    y1 = max(point[1] - templateSizeHalf, 0)
-    y2 = min(point[1] + templateSizeHalf, img1.shape[0] - 1)
-    subimage1 = np.copy(img1[y1:y2, x1:x2])
+    subimageF1, _ = get_and_pre_patch(img1, point, templateSizeHalf)
 
     # create subimage from img2 in which template is searched
-    subimageSizeHalf = int(subimageSize/2)
-    x1 = max(point[0] - subimageSizeHalf, 0)
-    x2 = min(point[0] + subimageSizeHalf, img2.shape[1] - 1)
-    y1 = max(point[1] - subimageSizeHalf, 0)
-    y2 = min(point[1] + subimageSizeHalf, img2.shape[0] - 1)
-    subimage2 = np.copy(img2[y1:y2, x1:x2])
+    subimageF2, offset = get_and_pre_patch(img2, point, subimageSizeHalf)
 
-    # preprocess both subimages
-    subimage1F = np.float32(subimage1)
-    subimage1F = lce(subimage1F, 11, 5)
-    subimage1F = cv2.cvtColor(subimage1F, cv2.COLOR_BGR2GRAY)
-    subimage1F = cv2.normalize(subimage1F, subimage1F, 0, 1, cv2.NORM_MINMAX)
-    subimage1F = cv2.GaussianBlur(subimage1F, (5,5), 0) 
-    subimage1X = cv2.Scharr(subimage1F, ddepth = -1, dx = 1, dy = 0)
-    subimage1Y = cv2.Scharr(subimage1F, ddepth = -1, dx = 0, dy = 1)
-    subimage1F = subimage1X + subimage1Y
-    subimage1F[subimage1F < 0.5] = 1 - subimage1F[subimage1F < 0.5]
-    #subimage1F = cv2.dilate(subimage1F, None)
-    #subimage1F = cv2.erode(subimage1F, None)
-    subimage1F = cv2.normalize(subimage1F, subimage1F, 0, 1, cv2.NORM_MINMAX)
+    # get possible corners
+    corners = cv2.goodFeaturesToTrack(subimageF2, 2000, qualityLevel=0.01, minDistance=10)
 
-    cv2.namedWindow("template", cv2.WINDOW_NORMAL)
-    cv2.imshow("template", np.uint8(cv2.normalize(subimage1F, subimage1F, 0, 255, cv2.NORM_MINMAX)))
-    cv2.resizeWindow("template", 640, 480)
+    # Scale patches, lines and points
+    sf = template_size_s/np.float32((subimageF1.shape[0] + subimageF1.shape[1])/2.)
+    subimageF1 = cv2.resize(subimageF1, (0,0), fx=sf, fy=sf)
+    subimageF2 = cv2.resize(subimageF2, (0,0), fx=sf, fy=sf)
+    corners_s = [corner[0] * sf for corner in corners]
+    templateSizeHalf_s = int(templateSizeHalf * sf)  
+    weights = np.full((corners.shape[0]), np.inf, dtype=np.float32)
+    patch = np.empty(subimageF1.shape)
+    i = 0
+    for corner in corners_s:
+        patch[:] = 0
+        xmin = int(corner[1]) - templateSizeHalf_s 
+        xmax = int(corner[1]) + templateSizeHalf_s+1
+        ymin = int(corner[0]) - templateSizeHalf_s
+        ymax = int(corner[0]) + templateSizeHalf_s+1
 
-    subimage2F = np.float32(subimage2)
-    subimage2F = lce(subimage2F, 11, 5)
-    subimage2F = cv2.cvtColor(subimage2F, cv2.COLOR_BGR2GRAY)
-    subimage2F = cv2.normalize(subimage2F, subimage2F, 0, 1, cv2.NORM_MINMAX)
-    subimage2F = cv2.GaussianBlur(subimage2F, (5,5), 0) 
-    subimage2X = cv2.Scharr(subimage2F, ddepth = -1, dx = 1, dy = 0)
-    subimage2Y = cv2.Scharr(subimage2F, ddepth = -1, dx = 0, dy = 1)
-    subimage2F = subimage2X + subimage2Y
-    subimage2F[subimage2F < 0.5] = 1 - subimage2F[subimage2F < 0.5]
-    #subimage2F = cv2.dilate(subimage2F, None)
-    #subimage2F = cv2.erode(subimage2F, None)
-    subimage2F = cv2.normalize(subimage2F, subimage2F, 0, 1, cv2.NORM_MINMAX)
+        if xmin >= 0 and ymin >= 0 and xmax <= subimageF2.shape[0] and\
+                                       ymax <= subimageF2.shape[1]:
+            weights[i] = np.sum((subimageF2[xmin:xmax, ymin:ymax] -\
+                                 subimageF1)**2)                              
 
-    cv2.namedWindow("subimage", cv2.WINDOW_NORMAL)
-    cv2.imshow("subimage", np.uint8(cv2.normalize(subimage2F, subimage2F, 0, 255, cv2.NORM_MINMAX)))
-    cv2.resizeWindow("subimage", 640, 480)
+#        idx_min = 0 if xmin >= 0 else abs(xmin)
+#        idy_min = 0 if ymin >= 0 else abs(ymin)
+#        idx_max = subimageF1.shape[0] if xmax < subimageF2.shape[0] else\
+#                  subimageF1.shape[0] - (xmax-subimageF2.shape[0])
+#        idy_max = subimageF1.shape[1] if ymax < subimageF2.shape[1] else\
+#                  subimageF1.shape[1] - (ymax-subimageF2.shape[1])
+#
+#        num_elem = (idx_max - idx_min) * (idy_max - idy_min)
+#
+#        weights[i] = np.sum((subimageF2\
+#                                [max(xmin,0):min(xmax,subimageF2.shape[0]),\
+#                                 max(ymin,0):min(ymax,subimageF2.shape[1])] -\
+#                             subimageF1[idx_min:idx_max,idy_min:idy_max])**2)/\
+#                     num_elem
+#        print weights[i]
+        i+=1
 
-    # template matching
-    # norms are missing in cv2 python wrapper
-    CV_TM_SQDIFF = 0
-    CV_TM_SQDIFF_NORMED = 1
-    CV_TM_CCORR = 2
-    CV_TM_CCORR_NORMED = 3
-    CV_TM_CCOEFF = 4
-    CV_TM_CCOEFF_NORMED = 5
-    templateResult = cv2.matchTemplate(subimage2F, subimage1F, CV_TM_CCOEFF_NORMED)
-    templateResult1 = cv2.normalize(templateResult, templateResult, 0, 1, cv2.NORM_MINMAX)
+    corner = corners[np.argmin(weights),0]
+    draw_circle(subimageF2, tuple(np.int_(corners_s[np.argmin(weights)])), (255))
+    cv2.imshow('corners', np.uint8(cv2.normalize(subimageF2, subimageF2, 0, 255, cv2.NORM_MINMAX)))
+    cv2.imshow('patch',  np.uint8(cv2.normalize(subimageF1, subimageF1, 0, 255, cv2.NORM_MINMAX)))
 
-    cv2.namedWindow("result", cv2.WINDOW_NORMAL)
-    cv2.imshow("result", np.uint8(cv2.normalize(templateResult1, templateResult1, 0, 255, cv2.NORM_MINMAX)))
-    cv2.resizeWindow("result", 640, 480)
 
-    while cv2.waitKey(0) != 99:
-        pass
+    return corner + offset
 
-    cv2.destroyWindow("subimage")
-    cv2.destroyWindow("template")
-    cv2.destroyWindow("result")
-
-    minVal, maxVal, minLoc , maxLoc = cv2.minMaxLoc(templateResult1)
-
-    point2 = (maxLoc[0] + templateSizeHalf, maxLoc[1] + templateSizeHalf)
-
-    returnPoint = (x1 + int(point2[0]), y1 + int(point2[1]))
-    return returnPoint
 
 def getPFromRectangleACorespondingP(img1, img2, point1, point2):
     """Wrapper for getPointFromRectangle and getCorespondingPoint.
