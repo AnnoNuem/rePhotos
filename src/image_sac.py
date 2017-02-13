5187c368409306ba20e48923adc26d73de79645b
aschaffland <aschaffland@uos.de>
2017-02-13 21:58:49 +0100

    bugfix correct template size

--- src/image_sac.py
+++ src/image_sac.py
@@ -4,9 +4,9 @@ from image_helpers import lce
 from image_helpers import draw_circle
 
 # scale factor for subimage size on which template matching is done
-SUBIMAGE_SIZE_SCALE = 0.5 #0.159  
+SUBIMAGE_SIZE_SCALE = 0.3 #0.159  
 # scale factor for template size
-TEMPLATE_SIZE_SCALE = 0.1
+TEMPLATE_SIZE_SCALE = 0.07
 # minimum size of user drawn rectangle to start point search else return middle point
 MIN_RECT_SIZE = 5
 
@@ -19,9 +19,6 @@ def getPointFromRectangle(img1, point1, point2):
     assert 0 <= point2[1] < img1.shape[0], "Point2 outside image"
     assert 0 <= point2[0] < img1.shape[1], "Point2 outside image"
     
-    #TODO replace for build with:
-    #assert point1[0] != point2[0], "X cordinates of rectangle corners are equal -> no rectangle"
-    #assert point1[1] != point2[1], "Y cordinates of rectangle corners are equal -> no rectangle"
     # if rectangle is to small return middlepoint of the two given points, assuming user 
     # wanted to select a single point and not draw rectangle
     if abs(point1[0] - point2[0]) < MIN_RECT_SIZE or abs(point1[1] - point2[1]) < MIN_RECT_SIZE:
@@ -65,11 +62,19 @@ def getPointFromRectangle(img1, point1, point2):
 
 def get_and_pre_patch(img, point, size_half):
     x1 = max(point[0] - size_half, 0)
-    x2 = min(point[0] + size_half, img.shape[1] - 1)
+    x2 = min(point[0] + size_half, img.shape[1])
     y1 = max(point[1] - size_half, 0)
-    y2 = min(point[1] + size_half, img.shape[0] - 1)
-    subimageF1 = cv2.cvtColor(np.float32(img[y1:y2, x1:x2]), cv2.COLOR_BGR2HSV)[...,2]
-    return cv2.GaussianBlur(subimageF1, (5,5), 0), np.array((x1, y1))   
+    y2 = min(point[1] + size_half, img.shape[0])
+    delta_x_min = point[0] - x1
+    delta_x_max = x2 - point[0]
+    delta_y_min = point[1] - y1
+    delta_y_max = y2 - point[1]
+    subimage = cv2.cvtColor(np.float32(img[y1:y2, x1:x2]), cv2.COLOR_BGR2HSV)[...,2].copy()
+#    subimage = cv2.normalize(subimage, subimage, 0, 255 , cv2.NORM_MINMAX)
+#    lutable = np.uint8(np.arange(8).repeat(32)*36.4286)
+#    subimage = np.float32(cv2.LUT(np.uint8(subimage), lutable))
+    subimage = cv2.normalize(subimage, subimage, 0.0, 1.0 , cv2.NORM_MINMAX)
+    return cv2.GaussianBlur(subimage, (5,5), 0), np.array((x1, y1)), np.array([delta_x_min, delta_x_max, delta_y_min, delta_y_max], dtype=np.float32)   
 
 def getCorespondingPoint(img1, img2, point, template_size_s=101):
     """Search for coresponding point on second image given a point in first image using template matching."""
@@ -91,34 +96,45 @@ def getCorespondingPoint(img1, img2, point, template_size_s=101):
                              SUBIMAGE_SIZE_SCALE)/2)
 
     # get template from img1 in which user draw
-    subimageF1, _ = get_and_pre_patch(img1, point, templateSizeHalf)
+    subimageF1, _, deltas = get_and_pre_patch(img1, point, templateSizeHalf)
 
     # create subimage from img2 in which template is searched
-    subimageF2, offset = get_and_pre_patch(img2, point, subimageSizeHalf)
+    subimageF2, offset, _ = get_and_pre_patch(img2, point, subimageSizeHalf)
 
     # get possible corners
     corners = cv2.goodFeaturesToTrack(subimageF2, 2000, qualityLevel=0.01, minDistance=10)
 
     # Scale patches, lines and points
-    sf = template_size_s/np.float32((subimageF1.shape[0] + subimageF1.shape[1])/2.)
-    subimageF1 = cv2.resize(subimageF1, (0,0), fx=sf, fy=sf)
-    subimageF2 = cv2.resize(subimageF2, (0,0), fx=sf, fy=sf)
+    sf = template_size_s/np.float32(np.max(subimageF1.shape[0:2]))
+    subimageF1 = cv2.GaussianBlur(cv2.resize(subimageF1, (0,0), fx=sf, fy=sf), (5,5), 0)
+    subimageF2 = cv2.GaussianBlur(cv2.resize(subimageF2, (0,0), fx=sf, fy=sf), (5,5), 0)
     corners_s = [corner[0] * sf for corner in corners]
+    deltas = np.int_(deltas * sf)
+
     templateSizeHalf_s = int(templateSizeHalf * sf)  
     weights = np.full((corners.shape[0]), np.inf, dtype=np.float32)
     patch = np.empty(subimageF1.shape)
     i = 0
+    min_w = np.inf
     for corner in corners_s:
-        patch[:] = 0
-        xmin = int(corner[1]) - templateSizeHalf_s 
-        xmax = int(corner[1]) + templateSizeHalf_s+1
-        ymin = int(corner[0]) - templateSizeHalf_s
-        ymax = int(corner[0]) + templateSizeHalf_s+1
+        xmin = int(corner[0]) - deltas[0]
+        xmax = int(corner[0]) + deltas[1] + 1
+        ymin = int(corner[1]) - deltas[2]
+        ymax = int(corner[1]) + deltas[3] + 1
 
-        if xmin >= 0 and ymin >= 0 and xmax <= subimageF2.shape[0] and\
-                                       ymax <= subimageF2.shape[1]:
-            weights[i] = np.sum((subimageF2[xmin:xmax, ymin:ymax] -\
-                                 subimageF1)**2)                              
+        if xmin >= 0 and ymin >= 0 and xmax <= subimageF2.shape[1] and\
+                                       ymax <= subimageF2.shape[0]:
+            weights[i] = np.sum((subimageF2[ymin:ymax, xmin:xmax] -\
+                                 subimageF1)**2, dtype=np.float32)                              
+
+#            if weights[i] < min_w:
+#                print weights[i]
+#                min_w = weights[i]
+#                cv2.imshow('patch',  np.uint8(subimageF1*255))
+#                draw_circle(subimageF2, tuple(np.int_(corner)), (1))
+#                cv2.imshow('patch2',  np.uint8(subimageF2[ymin:ymax, xmin:xmax]*255))
+#                k = cv2.waitKey(0)
+#                if k == 27: break
 
 #        idx_min = 0 if xmin >= 0 else abs(xmin)
 #        idy_min = 0 if ymin >= 0 else abs(ymin)
@@ -138,9 +154,8 @@ def getCorespondingPoint(img1, img2, point, template_size_s=101):
         i+=1
 
     corner = corners[np.argmin(weights),0]
-    draw_circle(subimageF2, tuple(np.int_(corners_s[np.argmin(weights)])), (255))
-    cv2.imshow('corners', np.uint8(cv2.normalize(subimageF2, subimageF2, 0, 255, cv2.NORM_MINMAX)))
-    cv2.imshow('patch',  np.uint8(cv2.normalize(subimageF1, subimageF1, 0, 255, cv2.NORM_MINMAX)))
+#    draw_circle(subimageF2, tuple(np.int_(corners_s[np.argmin(weights)])), (1))
+#    cv2.imshow('corners', np.uint8(cv2.normalize(subimageF2, subimageF2, 0, 255, cv2.NORM_MINMAX)))
 
 
     return corner + offset
