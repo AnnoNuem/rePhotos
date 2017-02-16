e977727cd4e27b36c9a03be96bfd553da6d984a9
aschaffland <aschaffland@uos.de>
2017-02-16 13:20:43 +0100

    hog working

--- src/image_sac.py
+++ src/image_sac.py
@@ -117,8 +117,8 @@ def getCorespondingPoint(img1, img2, point, template_size_s=101):
     deltas = np.int_(deltas * sf)
 
     templateSizeHalf_s = int(templateSizeHalf * sf)  
-    weights = np.zeros((corners.shape[0]), dtype=np.float32)
-    #weights = np.full((corners.shape[0]), np.inf, dtype=np.float32)
+#    weights = np.zeros((corners.shape[0]), dtype=np.float32)
+    weights = np.full((corners.shape[0]), np.inf, dtype=np.float32)
     patch = np.empty(subimageF1.shape)
     i = 0
     min_w = np.inf
@@ -126,11 +126,27 @@ def getCorespondingPoint(img1, img2, point, template_size_s=101):
 
     #cv2.imshow('i2', np.uint8(subimageF2*255))
     #cv2.imshow('i1', np.uint8(subimageF1*255))
-    subimageF2 = cv2.normalize(get_gabor(subimageF2)[...,2], subimageF2, 0, 1, cv2.NORM_MINMAX)
-    subimageF1 = cv2.normalize(get_gabor(subimageF1)[...,2], subimageF1, 0, 1, cv2.NORM_MINMAX)
+    #subimageF2 = cv2.normalize(get_gabor(subimageF2)[...,2], subimageF2, 0, 1, cv2.NORM_MINMAX)
+    #subimageF1 = cv2.normalize(get_gabor(subimageF1)[...,2], subimageF1, 0, 1, cv2.NORM_MINMAX)
 
-    dif_img = np.ones_like(subimageF1)
+    #dif_img = np.ones_like(subimageF1)
+    winSize = (64,64)
+    blockSize = (16,16)
+    blockStride = (8,8)
+    cellSize = (8,8)
+    nbins = 9
+    derivAperture = 1
+    winSigma = 4.
+    histogramNormType = 0
+    L2HysThreshold = 2.0000000000000001e-01
+    gammaCorrection = 0
+    nlevels = 64
+    hog = cv2.HOGDescriptor(winSize,blockSize,blockStride,cellSize,nbins,derivAperture,winSigma,
+                            histogramNormType,L2HysThreshold,gammaCorrection,nlevels)
+    #hog= cv2.HOGDescriptor()
 
+
+    i1 = hog.compute(np.uint8(subimageF1[...,2]*255), (8,8), (8,8))
 #    cv2.imshow('f2', np.uint8(subimageF2*255))
 #    cv2.imshow('f1', np.uint8(subimageF1*255))
 #    cv2.waitKey()
@@ -143,13 +159,18 @@ def getCorespondingPoint(img1, img2, point, template_size_s=101):
 
         if xmin >= 0 and ymin >= 0 and xmax <= subimageF2.shape[1] and\
                                        ymax <= subimageF2.shape[0]:
+
+            i2 = hog.compute(np.uint8(subimageF2[ymin:ymax, xmin:xmax,2]*255), (8,8), (8,8))
+            weights[i] = np.sum((i1 - i2)**2, dtype=np.float32)
+
+
             # sqdiff_normed minimum is best
             #weights[i] = np.sum((subimageF2[ymin:ymax, xmin:xmax] - subimageF1)**2, dtype=np.float32)/\
             #   np.sqrt(np.sum(subimageF2[ymin:ymax,xmin:xmax]**2, dtype=np.float32) * np.sum(subimageF1**2, dtype=np.float32))
 
             #crosscorelation_normed maximum is best
-            weights[i] = np.sum((subimageF2[ymin:ymax, xmin:xmax] * subimageF1), dtype=np.float32)/\
-                np.sqrt(np.sum(subimageF2[ymin:ymax,xmin:xmax]**2, dtype=np.float32) * np.sum(subimageF1**2, dtype=np.float32))
+#            weights[i] = np.sum((subimageF2[ymin:ymax, xmin:xmax] * subimageF1), dtype=np.float32)/\
+#                np.sqrt(np.sum(subimageF2[ymin:ymax,xmin:xmax]**2, dtype=np.float32) * np.sum(subimageF1**2, dtype=np.float32))
 
             # hamming distance
 #            dif_img[(subimageF2[ymin:ymax, xmin:xmax]-subimageF1)**2<.01] = 0
@@ -183,11 +204,12 @@ def getCorespondingPoint(img1, img2, point, template_size_s=101):
 #        print weights[i]
         i+=1
 
-    corner = corners[np.argmax(weights),0]
+    corner = corners[np.argmin(weights),0]
 #    draw_circle(subimageF2, tuple(np.int_(corners_s[np.argmin(weights)])), (1))
 #    cv2.imshow('corners', np.uint8(cv2.normalize(subimageF2, subimageF2, 0, 255, cv2.NORM_MINMAX)))
 
 
+#    return offset
     return corner + offset
 
 
